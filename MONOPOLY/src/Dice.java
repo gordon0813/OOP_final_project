ce17f1411159e10975f864042352e64994e3089a
B06505054 <50238785+B06505054@users.noreply.github.com>
2019-05-24 09:38:13 +0800

    Add files via upload

---
+++ MONOPOLY/src/Dice.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+import java.util.Random;
+public class Dice {
+	private int diceNum;
+	private String diceIcon;
+	public Dice(){
+		diceNum=1;
+		diceIcon="images/Dice/dieRed1.png";
+	}
+	public int setDice() {
+		diceNum = MonopolyMain.r.nextInt(6)+1;
+		System.out.println(diceNum);
+		return diceNum;
+	}
+	public String getDiceIcon() {
+		switch (diceNum) {
+		case 1:
+			diceIcon= "images/Dice/dieRed1.png";
+			break;
+		case 2:
+			diceIcon= "images/Dice/dieRed2.png";
+			break;
+		case 3:
+			diceIcon= "images/Dice/dieRed3.png";
+			break;
+		case 4:
+			diceIcon= "images/Dice/dieRed4.png";
+			break;
+		case 5:
+			diceIcon= "images/Dice/dieRed5.png";
+			break;
+		case 6:
+			diceIcon= "images/Dice/dieRed6.png";
+			break;
+		
+		}
+		return diceIcon;
+	}
+}
