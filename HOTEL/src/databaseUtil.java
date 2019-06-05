2eeb733a1a60aaaca06f90e58f29bafa771d684c
linenting <b06902023@ntu.edu.tw>
2019-06-06 00:10:42 +0900

    add method to databaseUtil.java

--- HOTEL/src/databaseUtil.java
+++ HOTEL/src/databaseUtil.java
@@ -1,19 +1,22 @@
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileReader;
-import java.io.InputStreamReader;
 import java.sql.*;
+import java.util.Calendar;
 
 import org.json.simple.*;
 import org.json.simple.parser.*;
 
+/*
+ * Remember to call functions 'buildConnection()' and 'initDatabase()' 
+ * 										at the beginning of your program.
+ */
 public class databaseUtil {
 	// MySQL
 	static Connection connect = null;
 	static Statement stmt = null;
-	static ResultSet rs = null;
+	static ResultSet results = null;
 
+	// build connection to MySQL database(user:root, password:root)
 	public static void buildConnection() {
 		// build connection to MySQL
 		try {
@@ -29,13 +32,14 @@ public class databaseUtil {
 		}
 	}
 
+	// read SQL Script to build tables
 	public static void initDatabase() {
 		// build database
 		BufferedReader fin = null;
 		try {
 			System.out.print("Building database...");
 
-			fin = new BufferedReader(new FileReader("hotel.sql"));
+			fin = new BufferedReader(new FileReader("buildTable.sql"));
 			String line = null;
 			StringBuffer sb = new StringBuffer();
 			while ((line = fin.readLine()) != null) {
@@ -56,82 +60,165 @@ public class databaseUtil {
 		}
 	}
 
-	public static String readJSONFile(String filename) {
-		String result = null;
+	// insert a user to table 'Users' by given User object
+	public static boolean insertUser(User newUser) {
+		String cmd = "INSERT INTO Users"
+						+ "(UID, password)" 
+						+ "VALUES"
+						+ "(" + newUser.getUserID() + ", " + newUser.getPassword() + ")";
+		
 		try {
-			File jsonfile = new File(filename);
-			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(jsonfile), "big5"));
-			StringBuilder sb = new StringBuilder();
-			String line = br.readLine();
-			while (line != null) {
-				sb.append(line);
-				line = br.readLine();
-			}
-			result = sb.toString();
-			br.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return result;
-	}
-
-	public static void buildTable() {
-		// parse json
-		// JSON parser object to parse jsonStr
-		JSONParser jsonParser = new JSONParser();
-		String jsonStr = readJSONFile("HotelList.json");
-		JSONArray hotelList = null;
-		try {
-			System.out.print("Parsing JSON...");
-
-			hotelList = (JSONArray) jsonParser.parse(jsonStr);
-
-			System.out.println("finish!");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		// build RoomList table
-		try {
-			System.out.print("Building RoomList table...");
-
-			for (Object h : hotelList) {
-				JSONObject hotel = (JSONObject) h;
-				JSONArray roomList = (JSONArray) hotel.get("Rooms");
-				for (Object r : roomList) {
-					JSONObject room = (JSONObject) r;
-					if (room.get("Number").toString() == "0")
-						continue;
-					String cmd = "INSERT INTO RoomList"
-							+ "(HotelID, HotelStar, StreetAddress, Locality, RoomType, RoomPrice, Number)" + "VALUES"
-							+ "(" + hotel.get("HotelID").toString() + ", " + hotel.get("HotelStar").toString() + " , "
-							+ "\"" + hotel.get("Street-Address").toString() + "\"" + ", " + "\""
-							+ hotel.get("Locality").toString() + "\"" + ", " + "\"" + room.get("RoomType").toString()
-							+ "\"" + ", " + room.get("RoomPrice").toString() + ", " + room.get("Number").toString()
-							+ ");";
-					try {
-						stmt.execute(cmd);
-					} catch (Exception e) {
-						e.getStackTrace();
-					}
-				}
-			}
-
-			System.out.println("finish!");
-		} catch (Exception e) {
+			stmt.execute(cmd);
+		} catch (SQLException e) {
 			e.getStackTrace();
-			System.exit(4);
+			return false;
 		}
+		return true;
 	}
+	
+	// get the certain User by given UserID
+	public static User getUser(String UID) {
+		String cmd = "SELECT * FROM Users WHERE UID=" + UID;
+		try {
+			results = stmt.executeQuery(cmd);
+			if (results.next()) {
+				return new User(results.getString("UID"), results.getString("password"));
+			} else {
+				System.out.println("No such User!!");
+				return new User();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return new User();
+	}
+	
+	// change the format from yyyy/mm/dd to yyyy-mm-dd
+	private static String DateFormat(String origin) {
+		return origin.replace('/', '-');
+	}
+	// insert a Order to table 'Orders' by given Order object
+	public static boolean insertOrder(Order newOrder) {
+		int[] RoomNums = newOrder.getDemand();
+		String cmd = "INSERT INTO Orders"
+						+ "(OrderID, UID, HotelID, SingleRoom, DoubleRoom, QuadRoom, CheckIn, CheckOut)" 
+						+ "VALUES("
+						+ newOrder.getID() + ", " 
+						+ "\"" + newOrder.getUserID() + "\"" + ", "
+						+ newOrder.getHotelID() + ", "
+						+ RoomNums[0] + ", "
+						+ RoomNums[1] + ", "
+						+ RoomNums[2] + ", "
+						+ "\'" + DateFormat(newOrder.getCheckInDate()) + "\'" + ", "
+						+ "\'" + DateFormat(newOrder.getCheckOutDate()) + "\'" + ");";
+		try {
+			stmt.execute(cmd);
+		} catch (SQLException e) {
+			e.getStackTrace();
+			return false;
+		}
+		return true;
+	}
+	
+	// get the certain OrderID by given OrderID
+	public static Order getOrderByOrderID(int OrderID) {
+		String cmd = "SELECT * FROM Orders WHERE OrderID=" + OrderID;
+		try {
+			results = stmt.executeQuery(cmd);
+			
+			if (results.next()) {
+				int[] Demand = {results.getInt("SingleRoom"), results.getInt("DoubleRoom"), results.getInt("QuadRoom")};
+				return new Order(results.getInt("OrderID"), 
+								 results.getString("UID"), 
+								 results.getInt("HotelID"), 
+								 results.getDate("CheckIn").toString().replace('-', '/'),
+								 results.getDate("CheckOut").toString().replace('-', '/'),
+								 Demand);
+			} else {
+				System.out.println("No such Order!!");
+				return new Order();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return new Order();
+	}
+	// get the certain OrderID by given UserID
+	public static Order[] getOrderByUserID(String UID) {
+		String cmd = "SELECT * FROM Orders WHERE UID=" + UID;
+		System.out.println(cmd);
 
-	public static void buildDatabase() {
+		try {
+			int len;
+			results = stmt.executeQuery(cmd);
+			results.last();
+			len = results.getRow();
+			results.first();
+			
+			if (len == 0) {
+				System.out.println("No such Order!!");
+				return null;
+			}
+			
+			Order[] retList = new Order[len];
+			int index = 0;
+			do {
+				int[] Demand = {results.getInt("SingleRoom"), results.getInt("DoubleRoom"), results.getInt("QuadRoom")};
+				retList[index++] = new Order(results.getInt("OrderID"), 
+										 results.getString("UID"), 
+										 results.getInt("HotelID"), 
+										 results.getDate("CheckIn").toString().replace('-', '/'),
+										 results.getDate("CheckOut").toString().replace('-', '/'),
+										 Demand);
+			} while(results.next());
+
+			return retList;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	// get the certain OrderID by given HotelID
+	public static Order[] getOrderByHotelID(int HotelID) {
+		String cmd = "SELECT * FROM Orders WHERE HotelID=" + HotelID;
+		try {
+			int len;
+			results = stmt.executeQuery(cmd);
+			results.last();
+			len = results.getRow();
+			results.first();
+			
+			if (len == 0) {
+				System.out.println("No such Order!!");
+				return null;
+			}
+			
+			Order[] retList = new Order[len];
+			int index = 0;
+			do {
+				int[] Demand = {results.getInt("SingleRoom"), results.getInt("DoubleRoom"), results.getInt("QuadRoom")};
+				retList[index++] = new Order(results.getInt("OrderID"), 
+										 results.getString("UID"), 
+										 results.getInt("HotelID"), 
+										 results.getDate("CheckIn").toString().replace('-', '/'),
+										 results.getDate("CheckOut").toString().replace('-', '/'),
+										 Demand);
+			} while(results.next());
+			return retList;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}	
+		
+	public static void main(String[] args) {
 		buildConnection();
 		initDatabase();
-		buildTable();
-	}
-
-	public static void main(String[] args) {
-		buildDatabase();
+		
 		try {
 			if (connect != null)
 				connect.close();
