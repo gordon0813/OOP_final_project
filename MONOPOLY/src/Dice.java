02744d4ca3ea83092275f91f794b7e014477c6b0
B06505054 <50238785+B06505054@users.noreply.github.com>
2019-06-11 17:27:56 +0800

    Update Dice.java

--- MONOPOLY/src/Dice.java
+++ MONOPOLY/src/Dice.java
@@ -1,38 +1,22 @@
+import java.util.concurrent.TimeUnit;
+
 public class Dice {
 	private int diceNum;
-	private String diceIcon;
+	public String[] diceIcon= {"images/Dice/dieRed1.png","images/Dice/dieRed2.png","images/Dice/dieRed3.png","images/Dice/dieRed4.png",
+			"images/Dice/dieRed5.png","images/Dice/dieRed6.png"};
 	public Dice(){
 		diceNum=1;
-		diceIcon="images/Dice/dieRed1.png";
+		//diceIcon="images/Dice/dieRed1.png";
 	}
 	public int setDice() {
 		diceNum = Game.r.nextInt(6)+1;
-		System.out.println(diceNum);
+		//System.out.println(diceNum);
 		return diceNum;
 	}
-	public String getDiceIcon() {
-		switch (diceNum) {
-		case 1:
-			diceIcon= "images/Dice/dieRed1.png";
-			break;
-		case 2:
-			diceIcon= "images/Dice/dieRed2.png";
-			break;
-		case 3:
-			diceIcon= "images/Dice/dieRed3.png";
-			break;
-		case 4:
-			diceIcon= "images/Dice/dieRed4.png";
-			break;
-		case 5:
-			diceIcon= "images/Dice/dieRed5.png";
-			break;
-		case 6:
-			diceIcon= "images/Dice/dieRed6.png";
-			break;
-		
-		}
-		return diceIcon;
+	public int getDice() {
+		return diceNum;
+	}
+	public String getDiceIcon(){
+		return diceIcon[diceNum-1];
 	}
 }
-
