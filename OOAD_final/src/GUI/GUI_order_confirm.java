57b11f0b30931a56e86f6ef72852bfed682a1728
B06505006 <b06505006@ntu.edu.tw>
2019-12-26 23:25:47 +0800

    pus

--- OOAD_final/src/GUI/GUI_order_confirm.java
+++ OOAD_final/src/GUI/GUI_order_confirm.java
@@ -22,9 +22,13 @@ import databaseException.nomoreRoom;
 
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 
 import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Date;
 import java.util.Properties;
 import java.util.Vector;
 
@@ -52,20 +56,21 @@ public class GUI_order_confirm {
 	private JTable table;
 	private JTable hotel_info;
 	private Order current_order = GUI_hotelpage.getOrdr();
+	private Plan current_plan = current_order.getPlan();
 
 	/**
 	 * Launch the application.
 	 */
-	
 
 	/**
 	 * Create the application.
 	 */
 	public GUI_order_confirm() {
-		if(GUI_order_manage.isFrom_record()) {
+		if (GUI_order_manage.isFrom_record()) {
+			System.out.println("isfrom");
 			current_order = GUI_order_manage.getChosen_order();
 		}
-		initialize();		
+		initialize();
 	}
 
 	/**
@@ -80,33 +85,30 @@ public class GUI_order_confirm {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		frame.setVisible(true);
-		
+
 		Properties p = new Properties();
 		p.put("text.today", "today");
 		p.put("text.month", "month");
 		p.put("text.year", "year");
-		
-		UtilDateModel modelin = new UtilDateModel();
-		Plan pp = current_order.getPlan();
-		//can't really turn localdate into date...
-		modelin.setYear(current_order.getPlan().getCheckInOutDate().getCheckin().getYear());
-		modelin.setMonth(current_order.getPlan().getCheckInOutDate().getCheckin().getMonthValue());
-		modelin.setDay(current_order.getPlan().getCheckInOutDate().getCheckin().getDayOfMonth());
+
+		UtilDateModel modelin = new UtilDateModel();		
+		Date in = java.sql.Date.valueOf(current_plan.getCheckInOutDate().getCheckin());
+		modelin.setValue(in);
 		JDatePanelImpl datePanelin = new JDatePanelImpl(modelin, p);
 		JDatePickerImpl datePickerin = new JDatePickerImpl(datePanelin, new DateComponentFormatter());
+		datePickerin.getJFormattedTextField().setText(in.toString());
 		datePickerin.setSize(130, 38);
 		datePickerin.setLocation(664, 93);
-		
 
 		UtilDateModel modelout = new UtilDateModel();
-		modelin.setYear(current_order.getPlan().getCheckInOutDate().getCheckout().getYear());
-		modelin.setMonth(current_order.getPlan().getCheckInOutDate().getCheckout().getMonthValue());
-		modelin.setDay(current_order.getPlan().getCheckInOutDate().getCheckout().getDayOfMonth());
+		Date out = java.sql.Date.valueOf(current_plan.getCheckInOutDate().getCheckout());
+		modelout.setValue(out);
 		JDatePanelImpl datePanelout = new JDatePanelImpl(modelout, p);
 		JDatePickerImpl datePickerout = new JDatePickerImpl(datePanelout, new DateComponentFormatter());
+		datePickerout.getJFormattedTextField().setText(out.toString());
 		datePickerout.setSize(130, 38);
 		datePickerout.setLocation(896, 93);
-		
+
 		frame.getContentPane().add(datePickerin);
 		frame.getContentPane().add(datePickerout);
 
@@ -116,13 +118,13 @@ public class GUI_order_confirm {
 		frame.getContentPane().add(roomnum);
 
 		JSpinner spinner_1 = new JSpinner();
-		spinner_1.setValue(current_order.getPlan().getRoomNum().getSingleNum());
+		spinner_1.setValue(current_plan.getRoomNum().getSingleNum());
 		spinner_1.setFont(new Font("SansSerif", Font.PLAIN, 16));
 		spinner_1.setBounds(634, 12, 85, 33);
-		frame.getContentPane().add(spinner_1);		
+		frame.getContentPane().add(spinner_1);
 
 		JSpinner spinner_4 = new JSpinner();
-		spinner_4.setValue(current_order.getPlan().getRoomNum().getQuadNum());
+		spinner_4.setValue(current_plan.getRoomNum().getQuadNum());
 		spinner_4.setFont(new Font("SansSerif", Font.PLAIN, 16));
 		spinner_4.setBounds(941, 10, 85, 33);
 		frame.getContentPane().add(spinner_4);
@@ -134,7 +136,7 @@ public class GUI_order_confirm {
 
 		JSpinner spinner_2 = new JSpinner();
 		spinner_2.setFont(new Font("SansSerif", Font.PLAIN, 16));
-		spinner_2.setValue(current_order.getPlan().getRoomNum().getDoubleNum());
+		spinner_2.setValue(current_plan.getRoomNum().getDoubleNum());
 		spinner_2.setBounds(782, 12, 85, 33);
 		frame.getContentPane().add(spinner_2);
 
@@ -142,30 +144,35 @@ public class GUI_order_confirm {
 		label_9.setFont(new Font("SansSerif", Font.PLAIN, 16));
 		label_9.setBounds(879, 16, 54, 27);
 		frame.getContentPane().add(label_9);
-		
+
 		JButton confirm = new JButton("\u78BA\u5B9A");
 		confirm.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				try {
-					LocalDate selectedDatein = (LocalDate) datePickerin.getModel().getValue();
-					LocalDate selectedDateout = (LocalDate) datePickerout.getModel().getValue();
-					RoomNum rn = new RoomNum((Integer) spinner_1.getValue(),
-							(Integer) spinner_2.getValue(), (Integer) spinner_4.getValue());
+					Date datein = (Date) (datePickerin.getModel().getValue());
+					Date dateout = (Date) datePickerout.getModel().getValue();
+					System.out.println(datein.toString());
+					LocalDate selectedDatein = datein.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+					LocalDate selectedDateout = dateout.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+					RoomNum rn = new RoomNum((Integer) spinner_1.getValue(), (Integer) spinner_2.getValue(),
+							(Integer) spinner_4.getValue());
 					CheckInOutDate ckio = new CheckInOutDate(selectedDatein, selectedDateout);
-					current_order.getPlan().setRoomNum(rn);
-					current_order.getPlan().setCheckInOutDate(ckio);
-					if(GUI_order_manage.isFrom_record()) {
+					current_plan.setRoomNum(rn);
+					current_plan.setCheckInOutDate(ckio);					
+					if (GUI_order_manage.isFrom_record()) {
 						try {
-							User.getUser().editOrder(current_order);
+							System.out.println("yes");
+							current_order.editOrder(current_plan);
 						} catch (Exception e1) {
 							// TODO Auto-generated catch block
 							e1.printStackTrace();
 						}
 						GUI_order_manage.setFrom_record(false);
-					}else{
+					} else {
 						try {
-							current_order.confirm();
+							current_plan.toOrder().confirm();
+							System.out.println(current_order);
 							frame.dispose();
 							GUI_user fre = new GUI_user();
 						} catch (noSuchHotel e1) {
@@ -191,7 +198,7 @@ public class GUI_order_confirm {
 		confirm.setFont(new Font("SansSerif", Font.PLAIN, 16));
 		confirm.setBounds(896, 613, 130, 29);
 		frame.getContentPane().add(confirm);
-		
+
 		JButton button_user = new JButton("\u4F7F\u7528\u8005");
 		button_user.addMouseListener(new MouseAdapter() {
 			@Override
@@ -202,40 +209,42 @@ public class GUI_order_confirm {
 		button_user.setFont(new Font("SansSerif", Font.PLAIN, 16));
 		button_user.setBounds(896, 572, 130, 29);
 		frame.getContentPane().add(button_user);
-		
+
 		JLabel label = new JLabel("\u7E3D\u50F9:");
 		label.setFont(new Font("SansSerif", Font.PLAIN, 16));
 		label.setBounds(577, 186, 62, 27);
 		frame.getContentPane().add(label);
-		
-		JLabel lblNewLabel = new JLabel(((Long)current_order.getPlan().calTotalPrice()).toString());
+
+		JLabel lblNewLabel = new JLabel(((Long) current_plan.calTotalPrice()).toString());
 		lblNewLabel.setFont(new Font("SansSerif", Font.PLAIN, 16));
 		lblNewLabel.setBounds(622, 186, 144, 27);
 		frame.getContentPane().add(lblNewLabel);
-		
+
 		JScrollPane scrollPane_2 = new JScrollPane();
 		scrollPane_2.setBounds(10, 15, 520, 197);
 		frame.getContentPane().add(scrollPane_2);
-		
+
 		JTable order_info = new JTable();
 		order_info.setFont(new Font("新細明體", Font.PLAIN, 20));
 		order_info.setBounds(10, 10, 542, 218);
 		frame.getContentPane().add(order_info);
-		
-		String[][] inf = {{"用戶",User.getUser().getname()},{"訂單號",((Long)current_order.getId()).toString()},{"飯店號",((Integer)current_order.getPlan().getHotel().getId()).toString()},
-				{"星級",((Integer)current_order.getPlan().getHotel().getStar()).toString()},
-				{"地址",current_order.getPlan().getHotel().getAddress()}};
-		String[] infhead = {"訂單資訊",""};
+
+		String[][] inf = { { "用戶", User.getUser().getname() }, { "訂單號", ((Long) current_order.getId()).toString() },
+				{ "飯店號", ((Integer) current_plan.getHotel().getId()).toString() },
+				{ "星級", ((Integer) current_order.getPlan().getHotel().getStar()).toString() },
+				{ "地址", current_order.getPlan().getHotel().getAddress() },
+				{ "原日期", current_plan.getCheckInOutDate().toString() },
+				{ "原訂房", current_plan.getRoomNum().toString() } };
+		String[] infhead = { "訂單資訊", "" };
 		hotel_info = new JTable(new DefaultTableModel(inf, infhead));
 		scrollPane_2.setViewportView(hotel_info);
 		hotel_info.setPreferredScrollableViewportSize(new Dimension(63, 63));
-				
-		
+
 		JLabel label_1 = new JLabel("\u5165\u4F4F\u65E5\u671F:");
 		label_1.setFont(new Font("SansSerif", Font.PLAIN, 16));
 		label_1.setBounds(577, 93, 77, 27);
 		frame.getContentPane().add(label_1);
-		
+
 		JLabel label_2 = new JLabel("\u9000\u623F\u65E5\u671F:");
 		label_2.setFont(new Font("SansSerif", Font.PLAIN, 16));
 		label_2.setBounds(804, 93, 77, 27);
