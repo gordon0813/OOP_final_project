5cc5968d588026e574cb90a4f38eb1f277fa6d53
B06505006 <b06505006@ntu.edu.tw>
2019-12-28 16:15:41 +0800

    yee

--- OOAD_final/src/GUI/GUI_hotelpage.java
+++ OOAD_final/src/GUI/GUI_hotelpage.java
@@ -1,4 +1,5 @@
 package GUI;
+
 import core.*;
 import databaseException.noSuchHotel;
 
@@ -11,9 +12,6 @@ import org.jdatepicker.impl.DateComponentFormatter;
 import org.jdatepicker.impl.JDatePanelImpl;
 import org.jdatepicker.impl.JDatePickerImpl;
 import org.jdatepicker.impl.UtilDateModel;
-
-
-
 
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
@@ -53,72 +51,78 @@ import java.net.URISyntaxException;
 import java.sql.SQLException;
 import java.awt.Font;
 import java.awt.Graphics;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.LineBorder;
 
 public class GUI_hotelpage {
-	//test data
-	LocalDate d1= LocalDate.of(2019, 12, 13);
-	LocalDate d2= LocalDate.of(2019, 12, 17);
-	private RoomNum rmnm = new RoomNum(3,3,3);
-	private CheckInOutDate ck = new CheckInOutDate(d1,d2);
-	private Hotel htl = new Hotel(2, 3, "road 1", new Room(1, 1000), new Room(2,200),new Room(4,30));
+	// test data
+	LocalDate d1 = LocalDate.of(2019, 12, 13);
+	LocalDate d2 = LocalDate.of(2019, 12, 17);
+	private RoomNum rmnm = new RoomNum(3, 3, 3);
+	private CheckInOutDate ck = new CheckInOutDate(d1, d2);
+	private Hotel htl = new Hotel(2, 3, "road 1", new Room(1, 1000), new Room(2, 200), new Room(4, 30));
 	private Plan test_plan = new Plan(rmnm, ck, htl);
 
 	private static JFrame frame;
+
 	public static JFrame getFrame() {
 		return frame;
 	}
+
 	private JTable table;
 	private Plan pln = GUI_search.getChosen_plan();
 	private static Order ordr;
 	private JTable hotel_info;
-    public static Order getOrdr() {
+
+	public static Order getOrdr() {
 		return ordr;
 	}
 
 	public static void setOrdr(Order ordr) {
 		GUI_hotelpage.ordr = ordr;
 	}
-	
-	
+
 	/**
 	 * Launch the application.
 	 */
-	
 
 	/**
 	 * Create the application.
-	 * @throws SQLException 
-	 * @throws noSuchHotel 
+	 * 
+	 * @throws SQLException
+	 * @throws noSuchHotel
 	 */
 	public GUI_hotelpage() throws noSuchHotel, SQLException {
-		if(GUI_record.isFrom_record()) {
+		if (GUI_record.isFrom_record()) {
 			pln = GUI_record.getChosen_plan();
 			GUI_record.setFrom_record(false);
-		}else if(GUI_order_manage.isFrom_record()) {
+		} else if (GUI_order_manage.isFrom_record()) {
 			ordr = GUI_order_manage.getChosen_order();
 			pln = ordr.getPlan();
 			GUI_order_manage.setFrom_record(false);
 		}
-		
+
 		initialize();
 	}
 
 	/**
 	 * Initialize the contents of the frame.
-	 * @throws SQLException 
-	 * @throws noSuchHotel 
+	 * 
+	 * @throws SQLException
+	 * @throws noSuchHotel
 	 */
 	private void initialize() throws noSuchHotel, SQLException {
 		frame = new JFrame();
 		try {
-		    final BufferedImage backgroundImage = ImageIO.read(new File("images/8.png"));
-		    frame.setContentPane(new JPanel(new BorderLayout()) {
-		        @Override public void paintComponent(Graphics g) {
-		            g.drawImage(backgroundImage, 0, 0, null);
-		        }
-		    });
+			final BufferedImage backgroundImage = ImageIO.read(new File("images/8.png"));
+			frame.setContentPane(new JPanel(new BorderLayout()) {
+				@Override
+				public void paintComponent(Graphics g) {
+					g.drawImage(backgroundImage, 0, 0, null);
+				}
+			});
 		} catch (IOException e) {
-		    throw new RuntimeException(e);
+			throw new RuntimeException(e);
 		}
 		frame.getContentPane().setForeground(Color.WHITE);
 		frame.getContentPane().setBackground(Color.WHITE);
@@ -135,12 +139,14 @@ public class GUI_hotelpage {
 		frame.getContentPane().add(roomnum);
 
 		JSpinner spinner_1 = new JSpinner();
+		spinner_1.setBackground(Color.BLACK);
 		spinner_1.setValue(pln.getRoomNum().getSingleNum());
 		spinner_1.setFont(new Font("SansSerif", Font.PLAIN, 16));
 		spinner_1.setBounds(634, 12, 85, 33);
-		frame.getContentPane().add(spinner_1);		
+		frame.getContentPane().add(spinner_1);
 
 		JSpinner spinner_4 = new JSpinner();
+		spinner_4.setBackground(Color.BLACK);
 		spinner_4.setValue(pln.getRoomNum().getQuadNum());
 		spinner_4.setFont(new Font("SansSerif", Font.PLAIN, 16));
 		spinner_4.setBounds(941, 10, 85, 33);
@@ -153,6 +159,7 @@ public class GUI_hotelpage {
 		frame.getContentPane().add(label_7);
 
 		JSpinner spinner_2 = new JSpinner();
+		spinner_2.setBackground(Color.BLACK);
 		spinner_2.setFont(new Font("SansSerif", Font.PLAIN, 16));
 		spinner_2.setValue(pln.getRoomNum().getDoubleNum());
 		spinner_2.setBounds(782, 12, 85, 33);
@@ -163,58 +170,59 @@ public class GUI_hotelpage {
 		label_9.setFont(new Font("SansSerif", Font.PLAIN, 16));
 		label_9.setBounds(879, 16, 54, 27);
 		frame.getContentPane().add(label_9);
-		
+
 		JLabel hyperlink = new JLabel("veiw on google map");
 		hyperlink.setForeground(Color.WHITE);
 		hyperlink.setFont(new Font("SansSerif", Font.PLAIN, 16));
-		hyperlink.setBounds(840, 116, 150, 27);
+		hyperlink.setBounds(700, 230, 150, 27);
 		hyperlink.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		frame.getContentPane().add(hyperlink);
 		hyperlink.addMouseListener(new MouseAdapter() {
-			 
-		    @Override
-		    public void mouseClicked(MouseEvent e) {
-		    	  try {    
-		    	        Desktop.getDesktop().browse(new URI(pln.getHotel().googlemapURL()));
-		    	         
-		    	    } catch (IOException | URISyntaxException e1) {
-		    	        e1.printStackTrace();
-		    	  }
-		    }
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				try {
+					Desktop.getDesktop().browse(new URI(pln.getHotel().googlemapURL()));
+
+				} catch (IOException | URISyntaxException e1) {
+					e1.printStackTrace();
+				}
+			}
 
 		});
 
 		String[] head = { "評論" };
-		String[] place = pln.getHotel().loadcomments();	
-		String[][] ph = {{}};
-		if(place != null) {
+		String[] place = pln.getHotel().loadcomments();
+		String[][] ph = { {} };
+		if (place != null) {
 			ph = new String[place.length][1];
-			for(int i = 0;i<place.length;i++) {
+			for (int i = 0; i < place.length; i++) {
 				ph[i][0] = place[i];
 			}
-		}			
-		else {
+		} else {
 			ph = new String[1][1];
-			ph[0][0] = "no comment";			
+			ph[0][0] = "no comment";
 		}
-			
+
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(10, 222, 835, 315);
+		scrollPane.setBounds(10, 280, 835, 340);
 		frame.getContentPane().add(scrollPane);
 		table = new JTable(new DefaultTableModel(ph, head));
+		table.setBackground(Color.BLACK);
+		table.setForeground(Color.WHITE);
 		scrollPane.setViewportView(table);
 		table.setPreferredScrollableViewportSize(new Dimension(450, 63));
 		table.setFillsViewportHeight(true);
 		table.setRowSelectionAllowed(true);
-		
+
 		JButton confirm = new JButton("\u78BA\u5B9A");
 		confirm.setBackground(Color.BLACK);
 		confirm.setForeground(Color.WHITE);
 		confirm.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				RoomNum rn = new RoomNum((Integer) spinner_1.getValue(),
-						(Integer) spinner_2.getValue(), (Integer) spinner_4.getValue());
+				RoomNum rn = new RoomNum((Integer) spinner_1.getValue(), (Integer) spinner_2.getValue(),
+						(Integer) spinner_4.getValue());
 				pln.setRoomNum(rn);
 				try {
 					ordr = pln.toOrder();
@@ -228,7 +236,7 @@ public class GUI_hotelpage {
 		confirm.setFont(new Font("SansSerif", Font.PLAIN, 16));
 		confirm.setBounds(896, 613, 130, 29);
 		frame.getContentPane().add(confirm);
-		
+
 		JButton button_user = new JButton("\u4F7F\u7528\u8005");
 		button_user.setBackground(Color.BLACK);
 		button_user.setForeground(Color.WHITE);
@@ -241,26 +249,28 @@ public class GUI_hotelpage {
 		button_user.setFont(new Font("SansSerif", Font.PLAIN, 16));
 		button_user.setBounds(896, 572, 130, 29);
 		frame.getContentPane().add(button_user);
-		
+
 		JLabel label = new JLabel("\u7E3D\u50F9:");
 		label.setForeground(Color.WHITE);
 		label.setFont(new Font("SansSerif", Font.PLAIN, 16));
 		label.setBounds(577, 92, 62, 27);
 		frame.getContentPane().add(label);
-		
-		JLabel lblNewLabel = new JLabel(((Long)pln.calTotalPrice()).toString());
+
+		JLabel lblNewLabel = new JLabel(((Long) pln.calTotalPrice()).toString());
+		lblNewLabel.setForeground(Color.WHITE);
 		lblNewLabel.setFont(new Font("SansSerif", Font.PLAIN, 16));
 		lblNewLabel.setBounds(623, 92, 144, 27);
 		frame.getContentPane().add(lblNewLabel);
-		
-		JScrollPane scrollPane_1 = new JScrollPane();
-		scrollPane_1.setBounds(10, 547, 563, 124);
-		frame.getContentPane().add(scrollPane_1);
-		
-		JTextArea textArea = new JTextArea();
-		textArea.setText("\u8A55\u8AD6...");
-		scrollPane_1.setViewportView(textArea);
-		
+		Plan pclone = pln;
+
+		JTextArea cmtarea = new JTextArea();
+		cmtarea.setForeground(Color.WHITE);
+		cmtarea.setBackground(Color.BLACK);
+		cmtarea.setText("\u8A55\u8AD6...");
+		cmtarea.setBounds(10, 140, 520, 120);
+		cmtarea.setBorder(new LineBorder(Color.WHITE, 3));
+		frame.getContentPane().add(cmtarea);
+
 		JButton comment = new JButton("\u8A55\u8AD6");
 		comment.setBackground(Color.BLACK);
 		comment.setForeground(Color.WHITE);
@@ -268,7 +278,7 @@ public class GUI_hotelpage {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				try {
-					pln.getHotel().addcomments(textArea.getText());
+					pln.getHotel().addcomments(cmtarea.getText());
 					GUI_order_manage fre = new GUI_order_manage();
 					frame.dispose();
 				} catch (UserException e1) {
@@ -284,18 +294,24 @@ public class GUI_hotelpage {
 			}
 		});
 		comment.setFont(new Font("SansSerif", Font.PLAIN, 16));
-		comment.setBounds(589, 642, 130, 29);
-		frame.getContentPane().add(comment);
-		
+		comment.setBounds(550, 230, 130, 29);
+		frame.getContentPane().add(comment, BorderLayout.SOUTH);
+
 		JScrollPane scrollPane_2 = new JScrollPane();
-		scrollPane_2.setBounds(10, 15, 520, 197);
+		scrollPane_2.setBounds(10, 15, 520, 110);
 		frame.getContentPane().add(scrollPane_2);
-				
-		String[][] inf = {{"飯店號",((Integer)pln.getHotel().getId()).toString()},{"星級",((Integer)pln.getHotel().getStar()).toString()},{"地址",pln.getHotel().getAddress()}};
-		String[] infhead = {"飯店資訊",""};
+
+		String[][] inf = { { "飯店號", ((Integer) pln.getHotel().getId()).toString() },
+				{ "星級", ((Integer) pln.getHotel().getStar()).toString() }, { "地址", pln.getHotel().getAddress() },
+				{ "原房數", pln.getRoomNum().toString() }, { "原總價", ((Long) pln.calTotalPrice()).toString() } };
+		String[] infhead = { "飯店資訊", "" };
 		hotel_info = new JTable(new DefaultTableModel(inf, infhead));
+		hotel_info.setRowSelectionAllowed(false);
+		hotel_info.setForeground(Color.WHITE);
+		hotel_info.setBackground(Color.BLACK);
 		scrollPane_2.setViewportView(hotel_info);
 		hotel_info.setPreferredScrollableViewportSize(new Dimension(63, 63));
+
 
 		// DefaultTableModel model = (DefaultTableModel) table.getModel();
 
