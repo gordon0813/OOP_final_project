e4027146754d26dd595680665277e832eef45a0c
B06505006 <b06505006@ntu.edu.tw>
2020-01-01 23:08:26 +0800

    ?

--- OOAD_final/src/GUI/GUI_login.java
+++ OOAD_final/src/GUI/GUI_login.java
@@ -95,7 +95,7 @@ public class GUI_login {
 			}
 		});
 		confirm.setFont(new Font("SansSerif", Font.PLAIN, 16));
-		confirm.setBounds(130, 136, 110, 29);
+		confirm.setBounds(485, 279, 110, 29);
 		confirm.setBackground(Color.BLACK);
         confirm.setForeground(Color.WHITE);
 		frame.getContentPane().add(confirm);
@@ -103,25 +103,25 @@ public class GUI_login {
 		JLabel label = new JLabel("\u7528\u6236\u540D");
 		label.setForeground(Color.WHITE);
 		label.setFont(new Font("新細明體", Font.PLAIN, 16));
-		label.setBounds(10, 22, 78, 20);
+		label.setBounds(365, 165, 78, 20);
 		frame.getContentPane().add(label);
 
 		username = new JTextField();
 		username.setFont(new Font("新細明體", Font.PLAIN, 16));
-		username.setBounds(77, 12, 249, 33);
+		username.setBounds(432, 155, 249, 33);
 		frame.getContentPane().add(username);
 		username.setColumns(10);
 
 		JLabel label_1 = new JLabel("\u5BC6\u78BC");
 		label_1.setForeground(Color.WHITE);
 		label_1.setFont(new Font("新細明體", Font.PLAIN, 16));
-		label_1.setBounds(10, 77, 78, 20);
+		label_1.setBounds(365, 220, 78, 20);
 		frame.getContentPane().add(label_1);
 
 		password = new JTextField();
 		password.setFont(new Font("新細明體", Font.PLAIN, 16));
 		password.setColumns(10);
-		password.setBounds(77, 71, 249, 33);
+		password.setBounds(432, 214, 249, 33);
 		frame.getContentPane().add(password);
 
 		JButton register = new JButton("\u8A3B\u518A");
@@ -144,7 +144,7 @@ public class GUI_login {
 			}
 		});
 		register.setFont(new Font("SansSerif", Font.PLAIN, 16));
-		register.setBounds(10, 136, 110, 29);
+		register.setBounds(365, 279, 110, 29);
 		frame.getContentPane().add(register);
 
 		JButton guest = new JButton("\u8A2A\u5BA2\u767B\u5165");
@@ -158,7 +158,7 @@ public class GUI_login {
 			}
 		});
 		guest.setFont(new Font("SansSerif", Font.PLAIN, 16));
-		guest.setBounds(250, 136, 110, 29);
+		guest.setBounds(605, 279, 110, 29);
 		frame.getContentPane().add(guest);
 
 	}
--- OOAD_final/src/GUI/GUI_order_confirm.java
+++ OOAD_final/src/GUI/GUI_order_confirm.java
@@ -263,6 +263,7 @@ public class GUI_order_confirm {
 		frame.getContentPane().add(label);
 
 		JLabel lblNewLabel = new JLabel(((Long) current_plan.calTotalPrice()).toString());
+		lblNewLabel.setForeground(Color.WHITE);
 		lblNewLabel.setFont(new Font("SansSerif", Font.PLAIN, 16));
 		lblNewLabel.setBounds(622, 186, 144, 27);
 		frame.getContentPane().add(lblNewLabel);
@@ -273,7 +274,7 @@ public class GUI_order_confirm {
 
 		JTable order_info = new JTable();
 		order_info.setFont(new Font("新細明體", Font.PLAIN, 20));
-		order_info.setBounds(10, 10, 542, 218);
+		order_info.setBounds(10, 10, 520, 140);
 		frame.getContentPane().add(order_info);
 
 		String[][] inf = { { "用戶", User.getUser().getname() }, { "訂單號", ((Long) current_order.getId()).toString() },
